fc12192ce144389f2167c2ca43fbf7738d847596
JaeWon <ljwon1995@naver.com>
2019-12-03 17:43:32 +0900

    Change code according to name change

--- PoliceAndThief/app/src/main/java/com/example/policeandthief/MainActivity.java
+++ PoliceAndThief/app/src/main/java/com/example/policeandthief/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class) ;
+                Intent intent = new Intent(MainActivity.this, ThiefActivity.class) ;
                 startActivity(intent) ;
             }
         });
