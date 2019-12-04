4a108b35e1d550e4fa120caeb5e9360555bf34c9
JaeWon <ljwon1995@naver.com>
2019-12-04 20:53:45 +0900

    BeepManager makes sound according to distance

--- PoliceAndThief/app/src/main/java/com/example/policeandthief/BeepManager.java
+++ PoliceAndThief/app/src/main/java/com/example/policeandthief/BeepManager.java
@@ -1,4 +1,31 @@
 package com.example.policeandthief;
 
-public class BeepManager {
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+
+public class BeepManager extends Thread{
+    private ToneGenerator tg;
+    private int distance = 1000;
+
+    public BeepManager(){
+        tg = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+    }
+
+    public void setBeep(float dist){
+        distance = (int)dist;
+    }
+
+    public void run(){
+
+        while(true) {
+            tg.startTone(ToneGenerator.TONE_CDMA_PIP, 100);
+            try {
+                Thread.sleep(distance * 10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
